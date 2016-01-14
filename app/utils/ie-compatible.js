9845a4bcc1cd0562fabfc1b73f9085cba7c645ca
pliman <sunbin235@163.com>
2016-01-14 22:19:59 +0800

    add bower.

--- app/utils/ie-compatible.js
+++ app/utils/ie-compatible.js
@@ -2,4 +2,4 @@ import 'es5-shim';
 import 'es5-shim/es5-sham';
 import 'console-polyfill';
 import '../../lib/respond/respond.modified.min'
-import '../../lib/html5shiv/html5shiv.min';
+import 'html5shiv';
